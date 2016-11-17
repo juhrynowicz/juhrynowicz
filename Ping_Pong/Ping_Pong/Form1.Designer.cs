fd23bf8bcc823513d649dbafc575b6d4e0c2fc2c
juhrynowicz <easyoke15@o2.pl>
2016-11-17 20:29:22 +0100

    Dodanie projektu
    Została już stworzona plansza do gry, obkiety(rakiety,piłeczka) oraz ich metody umożliwiające poruszane się

---
+++ Ping_Pong/Ping_Pong/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Ping_Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.gmOver = new System.Windows.Forms.Label();
+            this.Space = new System.Windows.Forms.Label();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Racket2 = new System.Windows.Forms.PictureBox();
+            this.Racket1 = new System.Windows.Forms.PictureBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.Score = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racket2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racket1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.BackColor = System.Drawing.SystemColors.ControlText;
+            this.playground.Controls.Add(this.gmOver);
+            this.playground.Controls.Add(this.Space);
+            this.playground.Controls.Add(this.Ball);
+            this.playground.Controls.Add(this.Racket2);
+            this.playground.Controls.Add(this.Racket1);
+            this.playground.Location = new System.Drawing.Point(0, 50);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(692, 577);
+            this.playground.TabIndex = 0;
+            // 
+            // gmOver
+            // 
+            this.gmOver.AutoSize = true;
+            this.gmOver.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gmOver.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.gmOver.Location = new System.Drawing.Point(276, 219);
+            this.gmOver.Name = "gmOver";
+            this.gmOver.Size = new System.Drawing.Size(91, 33);
+            this.gmOver.TabIndex = 4;
+            this.gmOver.Text = "gmOver";
+            // 
+            // Space
+            // 
+            this.Space.AutoSize = true;
+            this.Space.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Space.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Space.Location = new System.Drawing.Point(274, 148);
+            this.Space.Name = "Space";
+            this.Space.Size = new System.Drawing.Size(258, 33);
+            this.Space.TabIndex = 3;
+            this.Space.Text = "Press SPACE to continue";
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Ball.Location = new System.Drawing.Point(118, 304);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(20, 20);
+            this.Ball.TabIndex = 2;
+            this.Ball.TabStop = false;
+            // 
+            // Racket2
+            // 
+            this.Racket2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Racket2.Location = new System.Drawing.Point(250, 33);
+            this.Racket2.Name = "Racket2";
+            this.Racket2.Size = new System.Drawing.Size(200, 20);
+            this.Racket2.TabIndex = 1;
+            this.Racket2.TabStop = false;
+            // 
+            // Racket1
+            // 
+            this.Racket1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Racket1.Location = new System.Drawing.Point(250, 520);
+            this.Racket1.Name = "Racket1";
+            this.Racket1.Size = new System.Drawing.Size(200, 20);
+            this.Racket1.TabIndex = 0;
+            this.Racket1.TabStop = false;
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 30;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Score.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Score.Location = new System.Drawing.Point(12, 14);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(103, 33);
+            this.Score.TabIndex = 1;
+            this.Score.Text = "Player 1:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(567, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ":Player 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(542, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 33);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(112, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(692, 623);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racket2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racket1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.PictureBox Racket2;
+        private System.Windows.Forms.PictureBox Racket1;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Space;
+        private System.Windows.Forms.Label gmOver;
+    }
+}
+
---
+++ Ping_Pong/Ping_Pong/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ping_Pong
+{
+    public partial class Form1 : Form
+    {
+        private int speed_left = 2;
+        private int speed_top = 2;
+        private bool Player_Left1;
+        private bool Player_Right1;
+        private bool Player_Left2;
+        private bool Player_Right2;
+        private int score_player1=0;
+        private int score_player2=0;
+        private int max_score = 10;
+        public Form1()
+        {
+            InitializeComponent();
+            Timer.Enabled = true;
+            Cursor.Hide();
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            
+            
+            Space.Left = (playground.Width / 2) - (Space.Width/2);
+            Space.Top = (playground.Height / 2) - (Space.Height/2);
+            Space.Visible = false;
+            gmOver.Left = (playground.Width / 2) - (gmOver.Width / 2);
+            gmOver.Top = (playground.Height / 2) - (gmOver.Height / 2);
+            gmOver.Visible = false;
+            //Ustawienie szybkości przesuwania się graczy
+            if (Player_Right1 && Racket1.Left <= playground.Width - Racket1.Width)
+                Racket1.Left += 4;
+            if (Player_Left1 && Racket1.Left >= 0)
+                Racket1.Left -= 4;
+            if (Player_Right2 && Racket2.Left <= playground.Width - Racket1.Width)
+                Racket2.Left += 4;
+            if (Player_Left2 && Racket2.Left >= 0)
+                Racket2.Left -= 4;
+            Ball.Left += speed_left;
+            Ball.Top += speed_top;
+            if (Ball.Bounds.IntersectsWith(Racket1.Bounds))
+            {
+                speed_left += 1;
+                speed_top += 1;
+                speed_top = -speed_top;
+            }
+            if (Ball.Bounds.IntersectsWith(Racket2.Bounds))
+            {
+                speed_left -= 1;
+                speed_top -= 1;
+                speed_top = -speed_top;
+            }
+            if (Ball.Left <= playground.Left || Ball.Right >= playground.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (Ball.Bottom <= playground.Top)
+            {
+                this.score_player2 += 1;
+                this.label3.Text = score_player2.ToString();
+                Timer.Enabled = false;
+                Space.Visible = true;
+            }
+            if (Ball.Top >= playground.Bottom - 100)
+            {
+                this.score_player1 += 1;
+                this.label2.Text = score_player1.ToString();
+                Timer.Enabled = false;
+                Space.Visible = true;
+            }
+            if (score_player1 == max_score)
+            {
+                Space.Visible = false;
+                gmOver.Visible = true;
+                this.gmOver.Text = "GAME OVER\nPlayer 1 Won!\nPress F1 to restart\nESC to exit";
+            }
+            if (score_player2 == max_score)
+            {
+                Space.Visible = false;
+                gmOver.Visible = true;
+                this.gmOver.Text = "GAME OVER\nPlayer 2 Won!\nPress F1 to restart\nESC to exit";
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int[] speed = new int[] {-2,2};
+            Random rnd = new Random();
+            //Ustawienie przycisku wyjścia z gry "ESC"
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    Player_Right2 = false;
+                    Player_Left2 = true;
+                    break;
+                case Keys.Left:
+                    Player_Right1 = false;
+                    Player_Left1 = true;
+                    break;
+                case Keys.D:
+                    Player_Left2 = false;
+                    Player_Right2 = true;
+                    break;
+                case Keys.Right:
+                    Player_Left1 = false;
+                    Player_Right1 = true;
+                    break;
+            }
+            if (e.KeyCode == Keys.Space && Timer.Enabled == false)
+            {
+                this.speed_left = speed[rnd.Next(0,2)];
+                this.speed_top = speed[rnd.Next(0,2)];
+                Ball.Top = rnd.Next(250, 300);
+                Ball.Left = rnd.Next(250, 400);
+                Timer.Enabled = true;
+                Space.Visible = false;
+            }
+            if (e.KeyCode == Keys.F1 && Timer.Enabled == false && (score_player1 == max_score || score_player2 == max_score))
+            {
+                int index1 = rnd.Next(0, speed.Length-1);
+                int index2 = rnd.Next(0, speed.Length-1);
+                this.speed_left = speed[index1];
+                this.speed_top = speed[index2];
+                Ball.Top = rnd.Next(250,300);
+                Ball.Left = rnd.Next(250, 400);
+                this.score_player2 = 0;
+                this.score_player1 = 0;
+                Timer.Enabled = true;
+                Space.Visible = false;
+                gmOver.Visible = false;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    Player_Left2 = false;
+                    break;
+                case Keys.Left:
+                    Player_Left1 = false;
+                    break;
+                case Keys.D:
+                    Player_Right2 = false;
+                    break;
+                case Keys.Right:
+                    Player_Right1 = false;
+                    break;
+            }
+        }
+
+        
+
+    }
+}
